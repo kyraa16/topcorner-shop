cda895d2d226333c66612fdad3918bac7f0538d5
kyraa16 <kadekchandra16@gmail.com>
2025-09-16 08:45:30 +0700

    feat(main): data delivery using XML and JSON

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, crate_product, show_product
+from main.views import show_main, crate_product, show_product, show_products_xml, show_products_json, show_product_xml_by_id, show_product_json_by_id
 
 app_name = 'main'
 
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', crate_product, name='create_product'),
     path('product/<str:id>/', show_product, name='show_product'),
+    path('xml/', show_products_xml, name='show_products_xml'),
+    path('json/', show_products_json, name='show_products_json'),
+    path('xml/<str:id>/', show_product_xml_by_id, name='show_product_xml_by_id'),
+    path('json/<str:id>/', show_product_json_by_id, name='show_product_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product 
 from .forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
@@ -32,3 +34,32 @@ def show_product(request, id):
         'product': product
     }
     return render(request, 'product_detail.html', context)
+
+
+# Function to return products in XML Format
+def show_products_xml(request):
+    product_list = Product.objects.all()
+    xml_data = serializers.serialize('xml', product_list)
+    return HttpResponse(xml_data, content_type='application/xml')
+
+# Function to return products in JSON Format
+def show_products_json(request):
+    product_list = Product.objects.all()
+    json_data = serializers.serialize('json', product_list)
+    return HttpResponse(json_data, content_type='application/json') 
+
+def show_product_xml_by_id(request, id):
+    try:
+        product_item = Product.objects.filter(pk=id)
+        xml_data = serializers.serialize('xml', product_item)
+        return HttpResponse(xml_data, content_type='application/xml')
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
+
+def show_product_json_by_id(request, id):
+    try:
+        product_item = Product.objects.filter(pk=id)
+        json_data = serializers.serialize('json', product_item)
+        return HttpResponse(json_data, content_type='application/json')
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
